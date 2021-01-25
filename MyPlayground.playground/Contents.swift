5722f24ac1eec7c643b8707a06d1a92951fae44e
tuxubaev17 <tuxubaev17@mail.com>
2021-01-25 13:46:15 +0500

    taskss

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// MARK: 1
+//var array: [[Int]] = [[45, 12], [10, 19], [191, 3], [41, 81]]
+//
+//func sortByFirstAndSwap(_ array: inout [[Int]]) -> [[Int]] {
+//    for (index, element) in array.enumerated() {
+//        if (element[0] > element[1]) {
+//            (array[index][0], array[index][1]) = (array[index][1], array[index][0])
+//        }
+//    }
+//    return array
+//}
+//
+//
+//let sortedArray = sortByFirstAndSwap(&array)
+//
+//func decArray(arr: [[Int]]) -> [[Int]] {
+//    return sortedArray.sorted { $0[0] > $1[0] }
+//}
+//
+//print(decArray(arr: array))
+
+// MARK: 2
+
+//let inputString = "Hello world"
+//var dict : [Character: Int] = [:]
+//
+//let baseCount = zip(inputString, repeatElement(1, count: Int.max))
+//
+//dict.merge(baseCount, uniquingKeysWith: +)
+//print(dict)
+//
